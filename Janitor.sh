b3405ab780af850945bcb8b26af3af7eb44da989
Danny LyriCa <45947682+LyrixCaz@users.noreply.github.com>
2019-09-11 07:15:57 -0400

    Add files via upload
    Eden Workstation primary cronjob "Janitor" made for house keeping.

---
+++ Janitor.sh
@@ -0,0 +1,6 @@
+rm -rf ~/*.log *.png .bash_history
+rm -rf ~/.config/transmission-daemon/torrents/*.torrent
+rm -rdir --recursive -f ~/.cache .log
+rm -rf --recursive  -f ~/.local/share/Trash
+rm -rf ~/.local/share/applications/wine*
+rm -rdir ~/.local/share/applications/wine
