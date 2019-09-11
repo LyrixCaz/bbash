18bce46005f30eb9519190f362f075dda38cb530
Danny LyriCa <45947682+LyrixCaz@users.noreply.github.com>
2019-09-11 07:22:47 -0400

    Update Janitor.sh

--- Janitor.sh
+++ Janitor.sh
@@ -1,6 +1,9 @@
+# Eden Workstation's Janitor Worker 1.0 by Danny LyriCa
+# General House Keeping
 rm -rf ~/*.log *.png .bash_history
 rm -rf ~/.config/transmission-daemon/torrents/*.torrent
 rm -rdir --recursive -f ~/.cache .log
 rm -rf --recursive  -f ~/.local/share/Trash
+# Wine association cleanup
 rm -rf ~/.local/share/applications/wine*
 rm -rdir ~/.local/share/applications/wine
