1b1038da6c5334c6d1f87bfab2162a25dae36b98
Danny LyriCa <45947682+LyrixCaz@users.noreply.github.com>
2021-06-15 12:33:41 -0400

    Update Janitor.sh
    Fine tunning

--- Janitor.sh
+++ Janitor.sh
@@ -2,15 +2,15 @@
 # Janitor LV 0
 ##################################
 # Personal House Keeping
-rm ~/*.log *.png .bash_history
+# rm ~/*.log *.png .bash_history
 
 # Torrents
 rm ~/.config/transmission-daemon/torrents/*.torrent
 rm ~/.config/transmission/torrents/*.torrent
 
 # General House Keeping
-rm -rdir --recursive -f ~/.cache .log
+rm -rdir --recursive -f ~/.cache
 rm -rf --recursive  -f ~/.local/share/Trash
 
 # Wine association cleanup
-rm ~/.local/share/applications/wine*
+rm ~/.local/share/applications/wine-ext*
